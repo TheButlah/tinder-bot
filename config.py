f6e32bfac632643fcefa8fca1779bac5cae5579d
Ryan Butler <thebutlah@gmail.com>
2019-05-14 20:22:05 -0400

    Got a person finder working

---
+++ config.py
@@ -0,0 +1,5 @@
+fb_access_token = None  #fb_auth_token.get_fb_access_token(fb_username, fb_password)
+fb_user_id = None  # fb_auth_token.get_fb_id(fb_access_token)
+host = 'https://api.gotinder.com'
+#leave tinder_token empty if you don't use phone verification
+#tinder_token = None
